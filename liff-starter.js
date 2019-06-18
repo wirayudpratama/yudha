164000d02793ff16d1f693ae15bf325c95558def
yudarea <wirayudpratama111@gmail.com>
2019-06-18 11:17:39 +0700

    Add files via upload

---
+++ liff-starter.js
@@ -0,0 +1,202 @@
+window.onload = function (e) {
+    liff.init(function () {
+        getP();
+    });
+};
+
+function getP(){
+    var tipe = getParameterByName('type')
+    if (!tipe) {
+        document.getElementById('textx').addEventListener('click', function () {
+            liff.sendMessages([{
+                type: 'text',
+                text: 'line://app/1604506891-gLAxM4Ad?type=text&text=Your%20Text\n\n\ntype=text\ntext=your text'
+            }]).then(function () {
+                liff.closeWindow();
+            });
+        });
+        document.getElementById('imagex').addEventListener('click', function () {
+            liff.sendMessages([{
+                type: 'text',
+                text: 'line://app/1604506891-gLAxM4Ad?type=image&img=https://wallpaperstudio10.com/static/wpdb/wallpapers/1000x563/168891.jpg\n\ntype => image img => Link (must be HTTPS)'
+            }]).then(function () {
+                liff.closeWindow();
+            });
+        });
+        document.getElementById('videox').addEventListener('click', function () {
+            liff.sendMessages([{
+                type: 'text',
+                text: 'line://app/1604506891-gLAxM4Ad?type=video&ocu=https://tinyurl.com/y8og3or5&piu=https://images6.alphacoders.com/710/thumb-350-710132.png\n\ntype => video\nocu => video url piu => preview image'
+            }]).then(function () {
+                liff.closeWindow();
+            });
+        });
+        document.getElementById('audiox').addEventListener('click', function () {
+            liff.sendMessages([{
+                type: 'text',
+                text: 'line://app/1604506891-gLAxM4Ad?type=audio&link=https://platelets.fun/public/sounds/music.mp3'
+            }]).then(function () {
+                liff.closeWindow();
+            });
+        });
+        document.getElementById('mex').addEventListener('click', function () {
+            liff.sendMessages([{
+                type: 'text',
+                text: 'line://app/1604506891-gLAxM4Adj?type=profile'
+            }]).then(function () {
+                liff.closeWindow();
+            });
+        });
+        document.getElementById('stickerx').addEventListener('click', function () {
+            liff.sendMessages([{
+                type: 'text',
+                text: 'Animation: line://app/1604506891-gLAxM4Ad?type=sticker&stk=anim&sid=32128231&pkg=3099312\nNo Animation: line://app/1604506891-gLAxM4Ad?type=sticker&stk=noanim&sid=32128231&pkg=3099312\ntype => sticker stk => anim / noanim sid => sticker id\npkg => packages id'
+            }]).then(function () {
+                liff.closeWindow();
+            });
+        });
+    } else {
+        makeText();
+        makeImage();
+        makeVideo();
+        makeAudio();
+        makeSticker();
+        meProfile();
+    }
+    }
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+function getProfile(){
+    liff.getProfile().then(function (profile) {
+        document.getElementById('userid').textContent = 'Hai  ' + profile.displayName;
+        document.getElementById('main').src = profile.pictureUrl;        
+        document.getElementById('close').addEventListener('click', function () {
+            liff.closeWindow();
+        });
+    });
+}
+
+function makeText(){
+    var tipe = getParameterByName('type');
+    if (tipe === 'text') {
+        liff.sendMessages([{
+            type: 'text',
+            text: getParameterByName('text')
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    }
+}
+
+function makeImage(){
+    var tipe = getParameterByName('type');
+    if (tipe === 'image') {
+        liff.sendMessages([{
+            type: 'image',
+            originalContentUrl: getParameterByName('img'),
+            previewImageUrl: getParameterByName('img')
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    }
+}
+
+function makeVideo(){
+    var tipe = getParameterByName('type');
+    if (tipe === 'video') {
+        liff.sendMessages([{
+            type: 'video',
+            originalContentUrl: getParameterByName('ocu'),
+            previewImageUrl: getParameterByName('piu')
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    }
+}
+
+function makeAudio(){
+    var tipe = getParameterByName('type');
+    if (tipe === 'audio') {
+        liff.sendMessages([{
+            type: 'audio',
+            originalContentUrl: getParameterByName('link'),
+            duration: 60000
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    }
+}
+
+function makeSticker(){
+    var tipe = getParameterByName('type');
+    if (tipe === 'sticker') {
+        var stk = getParameterByName('stk');
+        var sid = getParameterByName('sid');
+        var pkg = getParameterByName('pkg');
+        var ep = '';
+        if (stk === 'anim') {
+            ep = "/IOS/sticker_animation@2x.png";
+        } else {
+            ep = "/IOS/sticker@2x.png";
+        }
+        liff.sendMessages([{
+          type: "template",
+          altText: "Sticker",
+          template: {
+             type: "image_carousel",
+             columns: [{
+                 imageUrl: "https://stickershop.line-scdn.net/stickershop/v1/sticker/"+sid+ep,
+                 action: {
+                     type: "uri",
+                     uri: "http://line.me/ti/p/%40175qduzr"}}
+                          ]
+                        }
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    }
+}
+
+function meProfile(){
+    var tipe = getParameterByName('type');
+    liff.getProfile().then(function (prof) {
+        var stat = prof.statusMessage;
+        if (stat == null) {
+            var stat = " - ";
+        }
+        if (stat.length > 100) {
+            var stat = "MAX STATUS MESSAGE ADITMADZS 100 WORDS";
+        }
+        if (tipe === 'profile') {
+            liff.sendMessages([{
+                type: "template",
+                altText: "Profile "+prof.displayName,
+                template: {
+                    type: "buttons",
+                    thumbnailImageUrl: prof.pictureUrl,
+                    imageAspectRatio: "square",
+                    imageSize: "cover",
+                    title: prof.displayName,
+                    text: stat,
+                    actions: [
+                        {
+                            type:"uri",
+                            label:"Me",
+                            uri:"line://app/1604506891-gLAxM4Ad?type=profile"
+                        }
+                    ]
+                }
+            }]).then(function () {
+                liff.closeWindow();
+            });
+        }
+    });
+}
